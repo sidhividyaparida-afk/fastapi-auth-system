a56a8db0f018fe363af7644568778714890489e0
S <sidhividyaparida@gmail.com>
2026-06-13 20:17:03 +0530

    nested model

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Address(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class User(BaseModel):
     name: str
     age: int
+    address: Address
 
 @app.post("/new user")
 def new_user(user: User):
