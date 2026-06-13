54d57d2a44afac2a61630acb270754a6095412b5
S <sidhividyaparida@gmail.com>
2026-06-13 19:39:15 +0530

    POST request

--- main.py
+++ main.py
@@ -3,22 +3,13 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# class User(BaseModel):
-#     name: str
-#     age: int
-
-# @app.post("/new user")
-# def new_user(user: User):
-#     return{
-#         "message": "User created successfully",
-#         "data": user
-#          }
+class User(BaseModel):
+    name: str
+    age: int
 
 @app.post("/new user")
-def new_user():
+def new_user(user: User):
     return{
         "message": "User created successfully",
-        "data": {"name": str,
-                 "king": str
-                 }
-                 }
+        "data": user
+         }
