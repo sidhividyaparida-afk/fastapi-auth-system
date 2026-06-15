ff933c27e0b49510745840b5de7407ab2af8efb1
S <sidhividyaparida@gmail.com>
2026-06-15 19:01:17 +0530

    combining path & query

--- main.py
+++ main.py
@@ -2,43 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+users = []
 
-todos = []
+class User(BaseModel):
+    name: str
+    age: int
 
-class Todo(BaseModel):
-    id: int
-    title: str
-    description: str
-    completed: bool
-# Create API 
-@app.post("/todos")
-def create_todo(todo: Todo):
-    todos.append(todo)
-    return {"message": "Todo created successfully", "todo": todo}
-# Read API
-@app.get("/todos")
-def get_todos():
-    return todos
-# get/read single data
-@app.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return todo
-    return {"error": "Todo not found"}
-# Update API
-@app.put("/todos/{todo_id}")
-def update_todo(todo_id: int, updated_todo: Todo):
-    for idx, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos[idx] = updated_todo
-            return {"message": "Todo updated successfully", "data": updated_todo}
-    return {"error": "Todo not found"}
-# Delete API
-@app.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int):
-    for idx, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(idx)
-            return {"message": "Todo deleted successfully"}
-    return {"error": "Todo not found"}
+@app.post("/users")
+def create_user(user: User):
+    users.append(user)
+    return {"message": "User created successfully", "user data": user}
+
+@app.get("/users")
+def get_users():
+    return {"users": users}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User, notify:bool = False):
+    if user_id < len(users):
+        users[user_id] = user
+        return {"message": "User updated successfully", "notify": notify, "user data": user}
+    return {"error": "User not found"}
